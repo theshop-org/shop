458f658aa82f55e3239381c3c65b20f01b0ebb77
levan <levanigavrilovi@192.168.0.104>
2024-08-03 18:27:31 +0400

    change size to 15

--- header.php
+++ header.php
@@ -391,7 +391,7 @@ $contact_us_email = get_field("contact_us_email", "option");
 						</svg>
 					</div>
 					<div class="grid-style" id="gridStyleTwo">  
-						<svg width="8" height="56" viewBox="0 0 9 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<svg width="8" height="15" viewBox="0 0 9 19" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<rect x="0.5" y="0.5" width="8" height="18" stroke="black"/>
 						</svg>
 						<svg width="8" height="15" viewBox="0 0 9 19" fill="none" xmlns="http://www.w3.org/2000/svg">
