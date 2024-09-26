f0b70dee4efa2ce066d883b7c666638052e7a4f5
GujaMikadze <mikadzeguja@gmail.com>
2024-09-26 20:07:43 +0400

    Remove lazy load

--- functions.php
+++ functions.php
@@ -828,13 +828,7 @@ function get_postcard_state_from_session() {
     }
 }
 
-function disable_lazy_load_for_mobile( $enabled, $context ) {
-    if ( wp_is_mobile() ) {
-        return false;
-    }
-    return $enabled;
-}
-add_filter( 'wp_lazy_loading_enabled', 'disable_lazy_load_for_mobile', 10, 2 );
+add_filter('wp_lazy_loading_enabled', '__return_false');
 
 ?>
 
