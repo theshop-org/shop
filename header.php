7adf1c49f44f19f51794a667a77484dae050d42f
levan <levanigavrilovi@192.168.0.103>
2024-10-09 01:26:50 +0400

    change 10 gel with $ 5 on postcard

--- header.php
+++ header.php
@@ -344,7 +344,7 @@ $contact_us_email = get_field("contact_us_email", "option");
 						</button>
 					</div>
 					<div class="post-card__right" style="align-self: flex-start; margin-left: auto; margin-top: 5px;">
-						10 GEL
+						$ 5
 					</div>
 					</label>
 					
